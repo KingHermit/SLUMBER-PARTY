f7a06423ab712c356ea2e9bf6af29bfb56882927
Reina Buen <rbuen34@gmail.com>
2025-11-29 23:21:40 -0800

    Refined Movement + Deleted PUN
    Currently don't need PUN so I got rid of it. Will add it later if I can't find a more refined system.
    - Also added Unity's Input System and it works much better for code.

--- SLUMBER PARTY!/Assets/Scripts/PlayerController.cs
+++ SLUMBER PARTY!/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerController : MonoBehaviour
@@ -10,12 +12,22 @@ public class PlayerController : MonoBehaviour
     // -- COMPONENTS
     [SerializeField] private BoxCollider2D b_collider;
     [SerializeField] private Rigidbody2D rb;
+    public InputActionReference move;
 
     // -- MOVEMENT STATS --
-    [SerializeField]
-    private float horizontal;
-    private float playerSpeed;
-    private float jumpForce;
+    [Header("Movement")]
+    [SerializeField] private float playerSpeed;
+    private Vector2 _moveDirection;
+
+    [Header("Jumping")]
+    [SerializeField] private float jumpForce;
+
+    [Header("Ground Check")]
+    private PlatformEffector2D effector;
+
+    [Header("Drop / Phase Through")]
+    [SerializeField] private float fallThroughDuration = 0.3f;
+    private bool fallingThrough = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,25 +40,69 @@ public class PlayerController : MonoBehaviour
         jumpForce = data.jumpForce;
     }
 
+    private IEnumerator FallThroughPlatform()
+    {
+        effector = GetCurrentPlatformEffector();
+
+        fallingThrough = true;
+
+        effector.rotationalOffset = 180f;
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.y, -5f); // apply downward force
+
+        yield return new WaitForSeconds(fallThroughDuration);
+
+        effector.rotationalOffset = 0;
+
+        fallingThrough = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        if (isGrounded() && Input.GetAxisRaw("Vertical") < 0 && !fallingThrough)
+        {
+            StartCoroutine(FallThroughPlatform());
+        }
+    }
 
-        // temp jumping controls
-        if (Input.GetKeyDown(KeyCode.Space))
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
 
+    public void Move(InputAction.CallbackContext context)
+    {
+        _moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    public void Drop(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded())
+        {
+            StartCoroutine("FallThroughPlatform");
+        }
+    }
+
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontal * playerSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(_moveDirection.x * playerSpeed, rb.linearVelocity.y);
     }
 
     private Boolean isGrounded()
     {
-        return rb.linearVelocity.y < 1;
+        return Physics2D.Raycast(transform.position, Vector2.down, 1.2f, LayerMask.GetMask("Platforms"));
     }
+
+    private PlatformEffector2D GetCurrentPlatformEffector()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.2f, LayerMask.GetMask("Platforms"));
+        if (hit.collider != null)
+            return hit.collider.GetComponent<PlatformEffector2D>();
+        return null;
+    }
+
 }
